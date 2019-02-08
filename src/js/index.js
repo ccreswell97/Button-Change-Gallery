be8bfd1b65ff269a0225bb10d97bed054468f86f
Chad Killingsworth <ckillingsworth@jackhenry.com>
2019-02-08 13:40:39 -0600

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+const BasicGallery = require('csc-adv-int-basic-gallery');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const base = document.getElementsByClassName('gallery')[0];
+  const containerWidth = base.getBoundingClientRect().width;
+  // 500 / 375 = containerWidth / X
+  const displayHeight = Math.round((containerWidth * 375) / 500);
+  base.style.height = "" + displayHeight + "px";
+  const gallery = new BasicGallery(base.getElementsByClassName('full-images')[0]);
+}, false);
