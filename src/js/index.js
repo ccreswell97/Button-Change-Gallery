7fad06d13c5c94103b5abb7bce5a8ad76911dbce
Carmen Creswell <ccreswell@adaptavist.com>
2019-02-13 22:53:46 -0600

    Add CSS and Implement event listeners on buttons

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,14 @@
 'use strict';
+'use strict';
 import {ThumbnailGallery} from './thumbnailgallery.js'
-
-const BasicGallery = require('csc-adv-int-basic-gallery');
 
 document.addEventListener('DOMContentLoaded', () => {
   const base = document.getElementsByClassName('gallery')[0];
+  const base2 = document.getElementsByClassName('thumbnail')[0];
   const containerWidth = base.getBoundingClientRect().width;
   // 500 / 375 = containerWidth / X
   const displayHeight = Math.round((containerWidth * 375) / 500);
   base.style.height = "" + displayHeight + "px";
-  //const gallery = new BasicGallery(base.getElementsByClassName('full-images')[0]);
-  const newgallery = new ThumbnailGallery(base.getElementsByClassName('small-images')[0]);
+  const gallery = new ThumbnailGallery(base.getElementsByClassName('full-images')[0]);
 }, false);
 
--- src/js/thumbnailgallery.js
+++ src/js/thumbnailgallery.js
@@ -3,10 +3,9 @@
 import BasicGallery from 'csc-adv-int-basic-gallery';
 
 export class ThumbnailGallery extends BasicGallery {
-    constructor(container, options) {
-
-        document.getElementById('right-button').addEventListener("click", function() {
-            document.getElementById('demo').innerHTML = "goodbye world";
-        })
+    constructor(container) {
+        super(container);
+        document.getElementById('left-button').addEventListener('click', this.prev.bind(this), false);
+        document.getElementById('right-button').addEventListener('click', this.next.bind(this), false);
     }    
 }
