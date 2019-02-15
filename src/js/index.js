deffce30166d1d931f1064a6dd201c31de94de14
Carmen Creswell <ccreswell@adaptavist.com>
2019-02-14 18:28:54 -0600

    Implement thumbnail event listeners

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,6 @@ import {ThumbnailGallery} from './thumbnailgallery.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   const base = document.getElementsByClassName('gallery')[0];
-  const base2 = document.getElementsByClassName('thumbnail')[0];
   const containerWidth = base.getBoundingClientRect().width;
   // 500 / 375 = containerWidth / X
   const displayHeight = Math.round((containerWidth * 375) / 500);
--- src/js/thumbnailgallery.js
+++ src/js/thumbnailgallery.js
@@ -7,5 +7,11 @@ export class ThumbnailGallery extends BasicGallery {
         super(container);
         document.getElementById('left-button').addEventListener('click', this.prev.bind(this), false);
         document.getElementById('right-button').addEventListener('click', this.next.bind(this), false);
+
+        document.getElementById('1').addEventListener('click', this.slide.bind(this, 0), false);
+        document.getElementById('2').addEventListener('click', this.slide.bind(this, 1), false);
+        document.getElementById('3').addEventListener('click', this.slide.bind(this, 2), false);
+        document.getElementById('4').addEventListener('click', this.slide.bind(this, 3), false);
+        document.getElementById('5').addEventListener('click', this.slide.bind(this, 4), false);
     }    
 }
